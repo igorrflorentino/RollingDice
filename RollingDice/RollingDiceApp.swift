703ef40825b7e9f2372c76a906a897b423cf1ca3
igorrflorentino <110570918+igorrflorentino@users.noreply.github.com>
2024-08-03 10:17:48 -0300

    Initial Commit

---
+++ RollingDice/RollingDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingDiceApp.swift
+//  RollingDice
+//
+//  Created by Igor Florentino on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RollingDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
